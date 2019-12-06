594a3532c5f83d4e2c8ff6cec0f5348d21687f7d
Estelle <estelle.allain@hotmail.fr>
2019-12-06 16:07:26 +0100

    first commit

---
+++ src/main/java/miage/EALL/bowlingScore/App.java
@@ -0,0 +1,13 @@
+package miage.EALL.bowlingScore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
